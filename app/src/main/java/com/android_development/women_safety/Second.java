f302d524cc04f675f9a03a9d6b569aad93dcbbe8
Vanshika521 <159027194+Vanshika521@users.noreply.github.com>
2024-10-14 23:16:30 +0530

    designing and new functionality

--- app/src/main/java/com/android_development/women_safety/Second.java
+++ app/src/main/java/com/android_development/women_safety/Second.java
@@ -1,24 +1,129 @@
+//package com.android_development.women_safety;
+//
+//import android.media.MediaPlayer;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.ImageButton;
+//import android.widget.Toast;
+//
+//
+//import androidx.activity.EdgeToEdge;
+//import androidx.annotation.StyleableRes;
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.core.graphics.Insets;
+//import androidx.core.view.ViewCompat;
+//import androidx.core.view.WindowInsetsCompat;
+//
+//public class Second extends AppCompatActivity {
+//
+//    ImageButton btn1,btn2,btn3,btn4,btn5;
+//MediaPlayer mediaPlayer;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_second);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+//
+//
+//        btn1 =findViewById(R.id.img1);
+//        btn2 =findViewById(R.id.img2);
+//        btn3 =findViewById(R.id.img3);
+//        btn4 =findViewById(R.id.img4);
+//        btn5 =findViewById(R.id.img5);
+//
+//        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.siren);
+//
+//
+//        btn3.setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View view) {
+//                System.out.println("1");
+//                Toast.makeText(Second.this, "Alarm clicked", Toast.LENGTH_SHORT).show();
+//
+//                if (mediaPlayer != null) {
+//                    mediaPlayer.stop();
+//                    mediaPlayer.reset(); // Reset to prepare for playback again
+//                    mediaPlayer.release();
+//                    Toast.makeText(Second.this, "Alarm ", Toast.LENGTH_SHORT).show();
+//                    // Release resources
+//                }
+//                return true;
+//            }
+//        });
+//
+//    }
+//}
+
 package com.android_development.women_safety;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Second extends AppCompatActivity {
+
+    ImageButton btn1, btn2, btn3, btn4, btn5;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_second);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btn1 = findViewById(R.id.img1);
+        btn2 = findViewById(R.id.img2);
+        btn3 = findViewById(R.id.img3);
+        btn4 = findViewById(R.id.img4);
+        btn5 = findViewById(R.id.img5);
+
+        // Initialize the MediaPlayer
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.siren);
+System.out.print("hlo");
+        btn3.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+
+            public boolean onLongClick(View view) {
+                System.out.println("1");
+                Toast.makeText(Second.this, "Alarm clicked", Toast.LENGTH_SHORT).show();
+
+                if (mediaPlayer != null) {
+                    mediaPlayer.stop();
+                    mediaPlayer.reset(); // Reset to prepare for playback again
+                    mediaPlayer.release(); // Release resources
+                    Toast.makeText(Second.this, "Alarm stopped", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release(); // Release resources when the activity is destroyed
+            mediaPlayer = null; // Avoid memory leaks
+        }
     }
 }
