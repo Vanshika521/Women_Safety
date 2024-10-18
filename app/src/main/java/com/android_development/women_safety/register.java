2886c6c9b0c1e0b28ad181b1a2143b89fe45bde9
Vanshika521 <159027194+Vanshika521@users.noreply.github.com>
2024-10-18 22:36:50 +0530

    new registration file designing

--- app/src/main/java/com/android_development/women_safety/register.java
+++ app/src/main/java/com/android_development/women_safety/register.java
@@ -1,6 +1,8 @@
 package com.android_development.women_safety;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class register extends AppCompatActivity {
+
+    EditText editText;
+    ImageButton btn1,btn2,btn3,btn4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,17 @@ public class register extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editText = findViewById(R.id.editText);
+        btn1 =findViewById(R.id.img1);
+        btn2 =findViewById(R.id.img2);
+        btn3 =findViewById(R.id.img3);
+        btn4 =findViewById(R.id.img4);
+
+
+        btn4.setOnClickListener();
+
     }
+
+
 }
